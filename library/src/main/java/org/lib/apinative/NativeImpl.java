9c643172b72fe01848b387ab87262c73a24fb885
Dave Syer <dsyer@pivotal.io>
2019-02-07 12:01:54 +0000

    Add thread

--- library/src/main/java/org/lib/apinative/NativeImpl.java
+++ library/src/main/java/org/lib/apinative/NativeImpl.java
@@ -5,7 +5,9 @@ import org.graalvm.nativeimage.c.function.CEntryPoint;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
 
 public final class NativeImpl {
+
 	private static JObject function;
+
 	private static JNIEnvironment env;
 
 	@CEntryPoint(name = "Java_org_pkg_apinative_Native_createIsolate", builtin = CEntryPoint.Builtin.CREATE_ISOLATE)
@@ -17,6 +19,13 @@ public final class NativeImpl {
 		NativeImpl.env = env;
 		NativeImpl.function = function;
 		System.err.println(process("foo"));
+		new Thread(() -> System.err.println(process("bar"))).start();
+		try {
+			Thread.sleep(1000L);
+		}
+		catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 	public static String process(String body) {
@@ -26,12 +35,19 @@ public final class NativeImpl {
 						.toCString("(Ljava/lang/Object;)Ljava/lang/Object;");
 				CTypeConversion.CCharPointerHolder input = CTypeConversion
 						.toCString(body);) {
+			System.err.println("6");
 			JClass cls = fn.getGetObjectClass().find(env, function);
+			System.err.println("5");
 			JMethodID apply = fn.getGetMethodID().find(env, cls, name.get(), sig.get());
+			System.err.println("4");
 			JValue args = StackValue.get(1, JValue.class);
+			System.err.println("3");
 			JString string = fn.getNewStringUTF().find(env, input.get());
+			System.err.println("2");
 			args.addressOf(0).l(string);
+			System.err.println("1");
 			JObject result = fn.getCallObjectMethodA().call(env, function, apply, args);
+			System.err.println("0");
 			return CTypeConversion.toJavaString(
 					fn.getGetStringUTFChars().find(env, (JString) result, false));
 		}
