d6e5a634e2597c90ef4e5834979c7c130f814e75
Dave Syer <dsyer@vmware.com>
2022-02-09 08:01:56 +0000

    Attempt to narrow in on toString

--- caller/src/main/java/org/pkg/apinative/Native.java
+++ caller/src/main/java/org/pkg/apinative/Native.java
@@ -1,7 +1,5 @@
 package org.pkg.apinative;
 
-import java.util.function.Function;
-
 public final class Native {
 
 	private static long isolate;
@@ -11,32 +9,17 @@ public final class Native {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("2 + 40 = " + add(2, 40));
-		System.out.println("12 + 30 = " + add(12, 30));
-		System.out.println("20 + 22 = " + add(20, 22));
-		System.out.println(run(foo -> new Foo(foo.getValue().toUpperCase()), new Foo("foo")));
+		print(new Foo("foo"));
 	}
 
-	public static int add(int a, int b) {
-		return add0(isolate, a, b);
+	public static <T> T print(T input) {
+		return print0(isolate, input);
 	}
 
-	public static <T> T run(Function<T,T> function, T input) {
-		return run0(isolate, function, input);
-	}
-
-	private static native int add0(long isolate, int a, int b);
-
-	private static native <T> T run0(long isolate, Function<?,?> function, Object foo);
+	private static native <T> T print0(long isolate, T foo);
 
 	private static native long createIsolate();
 
-	public static void hello(boolean z, char c, byte b, short s, int i, long j, float f,
-			double d) {
-		System.err.println("Hi, I have just been called back!");
-		System.err.print("With: " + z + " " + c + " " + b + " " + s);
-		System.err.println(" and: " + i + " " + j + " " + f + " " + d);
-	}
 }
 
 class Foo {
--- library/src/main/java/org/lib/apinative/NativeImpl.java
+++ library/src/main/java/org/lib/apinative/NativeImpl.java
@@ -8,50 +8,11 @@ public final class NativeImpl {
 	@CEntryPoint(name = "Java_org_pkg_apinative_Native_createIsolate", builtin = CEntryPoint.Builtin.CREATE_ISOLATE)
 	public static native long createIsolate();
 
-	@CEntryPoint(name = "Java_org_pkg_apinative_Native_add0")
-	static int add(JNIEnvironment env, JClass clazz,
-			@CEntryPoint.IsolateThreadContext long isolateId, int a, int b) {
+	@CEntryPoint(name = "Java_org_pkg_apinative_Native_print0")
+	static void print(JNIEnvironment env, JClass clazz,
+			@CEntryPoint.IsolateThreadContext long  isolateId, JObject object) {
 		JNINativeInterface fn = env.getFunctions();
-
-		try (CTypeConversion.CCharPointerHolder name = CTypeConversion.toCString("hello");
-				CTypeConversion.CCharPointerHolder sig = CTypeConversion
-						.toCString("(ZCBSIJFD)V");) {
-			System.err.println("Finding static method");
-			JMethodID helloId = fn.getGetStaticMethodID().find(env, clazz, name.get(),
-					sig.get());
-
-			JValue args = StackValue.get(8, JValue.class);
-			args.addressOf(0).z(false);
-			args.addressOf(1).c('A');
-			args.addressOf(2).b((byte) 22);
-			args.addressOf(3).s((short) 33);
-			args.addressOf(4).i(39);
-			args.addressOf(5).j(Long.MAX_VALUE / 2l);
-			args.addressOf(6).f((float) Math.PI);
-			args.addressOf(7).d(Math.PI);
-			System.err.println("Calling");
-			fn.getCallStaticVoidMethodA().call(env, clazz, helloId, args);
-		}
-
-		return a + b;
-	}
-
-	@CEntryPoint(name = "Java_org_pkg_apinative_Native_run0")
-	static JObject run(JNIEnvironment env, JClass clazz,
-			@CEntryPoint.IsolateThreadContext long  isolateId, JObject function,
-			JObject object) {
-		JNINativeInterface fn = env.getFunctions();
-		try (CTypeConversion.CCharPointerHolder name = CTypeConversion.toCString("apply");
-				CTypeConversion.CCharPointerHolder sig = CTypeConversion
-						.toCString("(Ljava/lang/Object;)Ljava/lang/Object;");) {
-			JClass cls = fn.getGetObjectClass().find(env, function);
-			JMethodID apply = fn.getGetMethodID().find(env, cls, name.get(), sig.get());
-			JValue args = StackValue.get(1, JValue.class);
-			args.addressOf(0).l(object);
-			System.err.println("Running: ");
-			JObject result = fn.getCallObjectMethodA().call(env, function, apply, args);
-			return result;
-		}
+		System.err.println("Running: " + string(env, object));
 	}
 
 	private static String string(JNIEnvironment env, JObject object) {
@@ -67,18 +28,6 @@ public final class NativeImpl {
 					.toJavaString(fn.getGetStringUTFChars().find(env, call));
 			return string;
 		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		System.err.println(NativeImpl.class.getMethod("hello", boolean.class, char.class)
-				.toGenericString());
-	}
-
-	public String getValue() {
-		return "value";
-	}
-
-	public static void hello(boolean z, char c) {
 	}
 
 }
